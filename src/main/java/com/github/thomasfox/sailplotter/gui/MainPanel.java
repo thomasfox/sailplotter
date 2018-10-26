8f8594f0014e8737769269bee23d0731dbd3edf0
Thomas Fox <fox@seitenbau.de>
2018-10-26 17:44:07 +0200

    add serialVersionUID

--- src/main/java/com/github/thomasfox/sailplotter/gui/MainPanel.java
+++ src/main/java/com/github/thomasfox/sailplotter/gui/MainPanel.java
@@ -8,6 +8,8 @@ import javax.swing.JPanel;
 
 public class MainPanel extends JPanel
 {
+  private static final long serialVersionUID = 1L;
+
   public MainPanel()
   {
     setLayout(new GridBagLayout());
