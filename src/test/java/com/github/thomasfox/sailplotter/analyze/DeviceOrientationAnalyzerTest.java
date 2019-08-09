dff122ed09f57ea137ee38db585f9d486b7bd30b
Thomas Fox <fox@seitenbau.de>
2019-08-09 10:24:41 +0200

    Code cosmetics

--- src/test/java/com/github/thomasfox/sailplotter/analyze/DeviceOrientationAnalyzerTest.java
+++ src/test/java/com/github/thomasfox/sailplotter/analyze/DeviceOrientationAnalyzerTest.java
@@ -1,6 +1,6 @@
 package com.github.thomasfox.sailplotter.analyze;
 
-import static  org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
